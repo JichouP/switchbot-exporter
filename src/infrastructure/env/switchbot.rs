3163e7f0ad5279f42e5ec5209fb22a7e7030cc70
JichouP <akkunn100@gmail.com>
2022-09-27 00:43:45 +0900

    :recycle: change unwrap to expect in SwitchBotEnv

--- src/infrastructure/env/switchbot.rs
+++ src/infrastructure/env/switchbot.rs
@@ -8,8 +8,8 @@ pub struct SwitchBotEnv {
 impl SwitchBotEnv {
     pub fn new() -> Self {
         Self {
-            token: env::var("SWITCH_BOT_TOKEN").unwrap(),
-            secret: env::var("SWITCH_BOT_SECRET").unwrap(),
+            token: env::var("SWITCH_BOT_TOKEN").expect("SWITCH_BOT_TOKEN is not set"),
+            secret: env::var("SWITCH_BOT_SECRET").expect("SWITCH_BOT_SECRET is not set"),
         }
     }
 }
