90522f066f071c25587e98ada948b79cc90e2024
JichouP <akkunn100@gmail.com>
2022-10-04 14:59:10 +0900

    :sparkles: add env

--- src/infrastructure/env/switchbot.rs
+++ src/infrastructure/env/switchbot.rs
@@ -3,6 +3,9 @@ use std::env;
 pub struct SwitchBotEnv {
     pub token: String,
     pub secret: String,
+    pub meter_plus_device_id: String,
+    pub plug_mini_pc_device_id: String,
+    pub plug_mini_desk_device_id: String,
 }
 
 impl SwitchBotEnv {
@@ -10,6 +13,12 @@ impl SwitchBotEnv {
         Self {
             token: env::var("SWITCH_BOT_TOKEN").expect("SWITCH_BOT_TOKEN is not set"),
             secret: env::var("SWITCH_BOT_SECRET").expect("SWITCH_BOT_SECRET is not set"),
+            meter_plus_device_id: env::var("SWITCH_BOT_METER_PLUS_DEVICE_ID")
+                .expect("SWITCH_BOT_METER_PLUS_DEVICE_ID is not set"),
+            plug_mini_pc_device_id: env::var("SWITCH_BOT_PLUG_MINI_PC_DEVICE_ID")
+                .expect("SWITCH_BOT_PLUG_MINI_PC_DEVICE_ID is not set"),
+            plug_mini_desk_device_id: env::var("SWITCH_BOT_PLUG_MINI_DESK_DEVICE_ID")
+                .expect("SWITCH_BOT_PLUG_MINI_DESK_DEVICE_ID is not set"),
         }
     }
 }
