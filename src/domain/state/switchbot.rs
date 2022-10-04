9f2869e455aa5358dae36082245bb198fdb7c323
JichouP <akkunn100@gmail.com>
2022-10-04 15:01:22 +0900

    :sparkles: add SwitchBotState

---
+++ src/domain/state/switchbot.rs
@@ -0,0 +1,72 @@
+use crate::domain::switchbot::get_devices_status::{
+    GetDevicesMeterPlusStatusResponse, GetDevicesMeterPlusStatusResponseBody,
+    GetDevicesPlugMiniStatusResponse, GetDevicesPlugMiniStatusResponseBody,
+};
+
+#[derive(Debug)]
+pub struct SwitchBotState {
+    pub meter_plus: MeterPlusState,
+    pub plug_mini_pc: PlugMiniState,
+    pub plug_mini_desk: PlugMiniState,
+}
+
+impl SwitchBotState {
+    pub fn new() -> Self {
+        Self {
+            meter_plus: MeterPlusState::default(),
+            plug_mini_pc: PlugMiniState::default(),
+            plug_mini_desk: PlugMiniState::default(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct MeterPlusState {
+    pub humidity: usize,
+    pub temperature: f64,
+}
+
+impl From<GetDevicesMeterPlusStatusResponse> for MeterPlusState {
+    fn from(v: GetDevicesMeterPlusStatusResponse) -> Self {
+        let GetDevicesMeterPlusStatusResponseBody {
+            humidity,
+            temperature,
+            ..
+        } = v.body;
+
+        Self {
+            humidity,
+            temperature,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct PlugMiniState {
+    pub power: String,
+    pub voltage: f64,
+    pub weight: f64,
+    pub electricity_of_day: usize,
+    pub electric_current: f64,
+}
+
+impl From<GetDevicesPlugMiniStatusResponse> for PlugMiniState {
+    fn from(v: GetDevicesPlugMiniStatusResponse) -> Self {
+        let GetDevicesPlugMiniStatusResponseBody {
+            power,
+            voltage,
+            weight,
+            electricity_of_day,
+            electric_current,
+            ..
+        } = v.body;
+
+        Self {
+            power,
+            voltage,
+            weight,
+            electricity_of_day,
+            electric_current,
+        }
+    }
+}
