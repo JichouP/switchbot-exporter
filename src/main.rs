31a80f2c65ed32f9fa2a61b05e3906a15e21667e
JichouP <akkunn100@gmail.com>
2022-10-10 23:08:20 +0900

    :sparkles: add liftoff printer

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate rocket;
 extern crate serde;
 
 use domain::state::switchbot::SwitchBotState;
+use rocket::fairing::AdHoc;
 use std::sync::{Arc, Mutex};
 use use_case::scheduler::setup_scheduler;
 
@@ -21,12 +22,23 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let rocket_handle = rocket::build()
         .manage(Arc::clone(&switch_bot_state))
+        .attach(AdHoc::on_liftoff("Liftoff Printer", |rocket| {
+            Box::pin(async {
+                println!(
+                    "Server is running on {}:{}",
+                    rocket.config().address,
+                    rocket.config().port
+                );
+            })
+        }))
         .mount("/status", route::status::status::build())
         .mount("/metrics", route::exporter::exporter::build())
         .launch();
 
     let (_, rocket_result) = futures::future::join(scheduler_handle, rocket_handle).await;
     let _rocket = rocket_result?;
+
+    println!("{}", _rocket.config().port);
 
     Ok(())
 }
